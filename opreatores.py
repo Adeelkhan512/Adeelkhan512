20d36597332397535de13a642b0ef24b4f4713d1
AdeelKhan512 <akc168799@gmail.com>
2024-10-08 20:38:17 +0500

    first commit

---
+++ opreatores.py
@@ -0,0 +1,170 @@
+a = 15
+b =10
+
+
+''' +  to add a and b
+- to subtract a from b
+* to multiply a and b
+/ to divide a and b
+// to floor divide a and b
+** to get a to power b
+% to get remainder'''
+
+# arthemetic opreator
+
+# addition
+print ("sum is :", a+b)
+
+# subtract
+print("sub is :",a-b)
+
+# multiplication
+print("mul is :", a*b)
+
+# division
+print("div is :", a/b)
+
+#floor division
+print("Floor div is :",a//b)
+
+# modulus
+print("Modulus is :",a %b)
+
+# power
+print("power is :",a**b)
+
+
+
+# Assignment opreator
+# assign 5 to x
+
+x = 5
+
+y = 10
+
+
+
+# assign the sum of x and y to x
+
+
+x +=y # x = x+y
+
+print(x)
+
+
+# sub 
+aa= 5
+aa -= 3
+print(aa) 
+
+# mul
+aa *=3
+print(aa)
+
+# div
+aa /=3
+print(aa)
+
+# floor div
+aa //=3
+print(aa)
+
+
+# power
+
+aa **=3
+print(aa)
+
+#3. Python comparison opreatores
+
+
+a = 5
+b = 2
+print(a<b)
+
+print(a>b)
+
+# equal operator
+print("a == b", a == b)
+
+
+# not equal to operator
+print(" a !=b",a !=b)
+
+# Greater than equal to
+print("a>=b", a>=b)
+
+
+# Less than equal to
+print("a<=b", a<=b)
+
+
+
+#4. python logical operators
+
+
+a = 5
+b = 6
+# logical AND
+print((a>2) and (b>=6))
+print(True and True) 
+print(True and False)
+
+# Logical OR
+print((a>2) or(b>=6))
+print(True or False)
+
+
+# Logical not
+print( not True)
+
+
+#5. Python Bitwise operatores
+# & Bitwise operator
+
+
+print(10 & 4)
+print( 10 | 4)
+print( 10 ^ 4)
+
+# 6. Python special operators
+# is operator
+
+x1 = 5
+y1 = 5
+x2 = "Python"
+y2 = "Python"
+x3 = [1,2,3,4]
+y3 = [1,2,3,]
+
+
+print(x1 is not y1)
+print(x2 is y2)
+print(x3 is y3)
+
+# Memebership operators
+
+# in
+# not in
+
+
+msg = "Hello Saim"
+dict = {1 : "a", 2 : "b"}
+
+
+# check Sa is present in msg string
+print("Sa in msg")
+
+print("Sa not in msg")
+
+
+print(1 in dict)
+print('a' in dict)
+
+
+
+
+
+
+
+
