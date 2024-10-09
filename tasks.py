4f8e1cd1a42aa08198b0b56e85fef9651cc3eb42
AdeelKhan512 <akc168799@gmail.com>
2024-10-09 19:25:31 +0500

    updates

---
+++ tasks.py
@@ -0,0 +1,27 @@
+# Task
+# Task 1
+
+length = 5
+width = 8
+
+area = length + width
+print("area of rectangle is :" , area)
+
+
+# Task 2
+base = 4
+height = 3
+area = 0.5*base*height
+print("area of triangle is :", area)
+
+# Task 3
+import math
+radius = 3
+
+area = math.pi *radius **3
+print("area of circle is :",area)
+
+# Task 4
+
+celsius_tem = 25
+fahrenheit_temp = 77
