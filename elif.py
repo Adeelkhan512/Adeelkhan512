d9f4a61fa86ac3419b4d906e1babf54e0b623bf5
AdeelKhan512 <akc168799@gmail.com>
2024-10-09 22:29:43 +0500

    elif

---
+++ elif.py
@@ -0,0 +1,43 @@
+# if condition
+  # code
+# elif condition
+     # code
+
+num = -18
+if num>0 :
+    print("positive number")
+elif num<0 :
+          print("negative number")
+else :
+       print("zero")
+
+# Python Nestad If
+number = -10
+
+if number > 0:
+       # inner
+        if number == 0:
+               print ("number is 0")
+        else:
+               print("number is positive")
+else:
+        print("Number is Negative")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tuple.py
@@ -0,0 +1,24 @@
+numbers = (1,2,4,6)
+print(numbers)
+
+
+# Charactericts
+# Immutable
+# ordered
+# Duplicate
+
+
+cars = ('BMW','AUDI','GWAGON','FORTUNER')
+
+
+print(cars)
+
+# Trying to modify given list
+
+cars[2] = 'COROLLA'
+
+print(len('cars'))
+
+
+for cars in cars :
+    print('car')
